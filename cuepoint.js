0fffd310a23fca39eae8baa75556ef5cc91143e6
Steve Preston <spreston@bcgtrans.com>
2013-08-21 21:56:52 -0400

    Updated readme

--- cuepoint.js
+++ cuepoint.js
@@ -1,67 +1,89 @@
-
 (function() {
-	/* Cuepoint Coffee. A simple library for HTML5 Video Subtitles and Cuepoints */
-	
-	/**
-	 * @class Utils 
-	*/
-	
+
 	var Cuepoint, Utils, utils;
+
 	Utils = (function() {
+
 		function Utils() {}
+
 		Utils.prototype.log = function(args) {
+
 			this.args = args;
+
 			if (window.console) {
 				return console.log(Array.prototype.slice.call(this, arguments));
 			}
+
 		};
+
 		return Utils;
+
 	})();
 	
-	/**
-	 * @class Cuepoint
-	 */
-	
 	Cuepoint = (function() {
+
 		function Cuepoint() {
+
 			this.nativeKeys = Object.keys;
+
 		}
+
         Cuepoint.prototype.SUBTITLE = 1;
         Cuepoint.prototype.TOGGLE_HTML = 2;
+
 		Cuepoint.prototype.init = function (slides, video, slideType) {
+
           var key, value, _results, elements;
+
           this.slides = slides;
           this.video = document.getElementById(video);
           this.subtitles = document.getElementById("subtitles");
+
           _results = [];
           elements = [];
+
           for (key in slides) {
+
             value = slides[key];
             elements.push(value);
+
             if (slideType == this.SUBTITLE){
                 this.addSlide(key, value);
             }else if (slideType == this.TOGGLE_HTML){
                 this.addCuepoint(key, value);
             } 
+
             _results.push(this.events.call);
+
           }
+
           this.elements = elements;
+
           return _results;
+
         };
+
         Cuepoint.prototype.addCuepoint = function (time, element) {
+
           var self;
+
           this.time = time;
           this.element = element;
           self = this;
+
           return this.video.addEventListener("timeupdate", function () {
             if (this.currentTime >= time && this.currentTime <= time + 0.3) {
               return self.cuepointUpdate(element);
             }
           }, false);
+
         };
+
         Cuepoint.prototype.hideAll = function () {
+
           var elementString = "";
           var space = "";
+
           for (key in this.elements){
             elementString += space + this.elements[key];
             space = ", ";
@@ -70,22 +92,28 @@
           $(elementString).hide();
           $(elementString).removeClass("cuepoint-visible");
           $(elementString).addClass("cuepoint-hidden");
+
           return this;
 
         };
+
 		Cuepoint.prototype.events = function() {};
+
 		Cuepoint.prototype.currentTime = function() {
 			return this.video.currentTime;
 		};
+
 		Cuepoint.prototype.update = function(html) {
 			this.html = html;
 			return this.subtitles.innerHTML = this.html;
 		};
+
 		Cuepoint.prototype.setTime = function(time) {
 			this.time = time;
 			this.video.currentTime = time;
 			return this.video.play();
 		};
+
 		Cuepoint.prototype.addSlide = function(time, html) {
 			var self;
 			this.time = time;
@@ -98,6 +126,7 @@
 			},
 			false);
 		};
+
         Cuepoint.prototype.cuepointUpdate = function (element) {
           this.hideAll();
           $(element).show();
@@ -107,17 +136,19 @@
 		Cuepoint.prototype.play = function() {
 			return this.video.play();
 		};
+
 		Cuepoint.prototype.pause = function() {
 			if (!this.video.paused) {
 				return this.video.pause();
 			}
 		};
+
 		return Cuepoint;
+
 	})();
+
 	utils = new Utils;
 	window.cuepoint = new Cuepoint;
+
 }).call(this);
-
-
-
 
--- readme.md
+++ readme.md
@@ -1,18 +1,31 @@
-# Cuepoint
+# Cuepoint Documentation (Updated)
+
 ## A javascript plugin for HTML5 video cuepoints and subtitles
 
-www.owainlewis.com
+Original Plugin Author URL: www.owainlewis.com
 
 License: MIT
 http://www.opensource.org/licenses/mit-license.php
+
+I modified this plugin to take a list of jquery selectors so that it could show and hide HTML elements when a cuepoint is reached. It can still take the original HTML slides and subtitles.
 
 ##Quick Start
 
 Simply pass in an object containing your slides to the cuepoint.init method. The time is in seconds
 
+    /**
+     * .content4 and .content 5 will be handled by #video2. Any number greater than the length of the video used will cause an element to be hidden and not shown.
+     */
 	$(document).ready(function(){
-	  var slides = {5:"Hello World"}
-	  cuepoint.init(slides);
+      var video = "video1";
+	  var slides = {
+        5: ".content1",
+        10: ".content2, .content3",
+        15: "#video2",
+        999: ".content4",
+        999: ".content5"
+      }
+	  cuepoint.init(slides, video, cuepoint.TOGGLE_HTML);//for original functionality use the cuepoint.SUBTITLE constant
 	  cuepoint.play();
 	});
 
@@ -21,4 +34,5 @@ If you want to create skip to links you can use the setTime(seconds) method and 
 	$('a').click(function(){
 	  cuepoint.setTime(60); // Jump to 60 seconds into the video
 	});
+
 
